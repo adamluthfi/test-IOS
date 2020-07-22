3ba046d7707da2bd3d9f88d83a42b73cbec71b87
adam.muzadid <adam.muzadid@mncgroup.com>
2020-07-22 10:16:20 +0700

    Initial Commit

---
+++ iOS Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Test
+//
+//  Created by Adam Lutpi Muzadid on 22/07/20.
+//  Copyright © 2020 Adam Lutpi Muzadid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
